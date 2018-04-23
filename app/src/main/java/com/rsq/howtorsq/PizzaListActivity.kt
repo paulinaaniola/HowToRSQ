e0765ffc094e0ba7b659d482d00aa15f5a00bfbd
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2018-04-23 19:36:57 +0200

    inital commit

---
+++ app/src/main/java/com/rsq/howtorsq/PizzaListActivity.kt
@@ -0,0 +1,12 @@
+package com.rsq.howtorsq
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PizzaListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_list)
+    }
+}
