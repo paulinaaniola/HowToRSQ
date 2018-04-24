f2af4e9ea4b99be0cd3ebd178a80dc4a658da11d
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2018-04-24 13:54:57 +0200

    create pizza object, create list, onStart to debug list and evaluate expressions

---
+++ app/src/main/java/com/rsq/howtorsq/Pizza.kt
@@ -0,0 +1,7 @@
+package com.rsq.howtorsq
+
+data class Pizza(
+        var name: String,
+        var price: Int,
+        var ingredients: String
+)
--- app/src/main/java/com/rsq/howtorsq/PizzaListActivity.kt
+++ app/src/main/java/com/rsq/howtorsq/PizzaListActivity.kt
@@ -5,9 +5,19 @@ import android.os.Bundle
 
 class PizzaListActivity : Activity() {
 
+    private val listOfPizza: MutableList<Pizza> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pizza_list)
+
+        listOfPizza.add(Pizza("Margarita", 12, "Ser, pomidor"))
+        listOfPizza.add(Pizza("Pepperoni", 12, "Ser, pomidor, pepperoni"))
+        listOfPizza.add(Pizza("Kebab", 12, "Ser, pomidor, kebab"))
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
 }
