34644504070d1b87a9480ae90099a408270ee449
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2018-04-24 16:40:45 +0200

    put and get extra, show pizza details

--- app/src/main/java/com/rsq/howtorsq/PizzaDetailsActivity.kt
+++ app/src/main/java/com/rsq/howtorsq/PizzaDetailsActivity.kt
@@ -2,13 +2,27 @@ package com.rsq.howtorsq
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pizza_details.*
 
 class PizzaDetailsActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pizza_details)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        val pizza = intent.getSerializableExtra("pizza")
+        if (pizza != null && pizza is Pizza) {
+            setupPizzaDetails(pizza)
+        }
+    }
+
+    private fun setupPizzaDetails(pizza: Pizza) {
+        nameTextView.text = pizza.name
+        priceTextView.text = pizza.price.toString()
+        ingredientsTextView.text = pizza.ingredients
     }
 
 }
